9c8470bf6b633f2d30eef29c2a4e8b0cbfbbb107
luizlobatojunior <luizlobatojr@gmail.com>
2025-02-07 12:22:10 +0000

    day of code

---
+++ DayofCode002.js
@@ -0,0 +1,24 @@
+/*- Qual o seu nome?
+- Quantos anos você tem?
+- Qual linguagem de programação você está estudando?
+
+Olá [nome], você tem [idade] anos e já está aprendendo [linguagem]!"
+EXERCÍCIO OPCIONAL
+Se você quiser se aprofundar no assunto de hoje, eu tenho mais um exercício para você.
+
+Mas ele é 100% opcional.
+
+Você vai complementar o código para que, depois de exibir a mensagem anterior, o programa pergunte:
+
+Você gosta de estudar [linguagem]? Responda com o número 1 para SIM ou 2 para NÃO.
+
+E aí, dependendo da resposta, ele deve mostrar uma das seguintes mensagens:
+
+1 > Muito bom! Continue estudando e você terá muito sucesso.
+2 > Ahh que pena... Já tentou aprender outras linguagens?*/
+
+let nome = prompt("Qual é seu nome?");
+let idade = prompt("Quantos anos você tem?");
+let linguagem = prompt("Qual linguagem de programação você está aprendendo?");
+
+console.log(`Seu nome é ${nome}, você tem ${idade} anos e está aprendendo ${linguagem}.`)
